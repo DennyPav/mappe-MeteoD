b1262b609a0d0c0d250f5f7b1d89cc15f0b30f69
Deniel Pavone <150160272+DennyPav@users.noreply.github.com>
2026-01-26 03:45:21 +0100

    Reduce image quality from 80 to 70

--- run_icon.py
+++ run_icon.py
@@ -342,7 +342,7 @@ def save_plot(name):
         name, 
         dpi=120, 
         bbox_inches="tight", 
-        pil_kwargs={'quality': 80}
+        pil_kwargs={'quality': 70}
     )
     plt.close()
 
